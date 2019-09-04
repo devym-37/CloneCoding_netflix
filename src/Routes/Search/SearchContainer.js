f55eb99deb2c95906e49178407539a725caa69e0
miniyoung37 <miniyoung37@gmail.com>
2019-09-04 21:13:39 +0900

    searchcontainer 수정

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
+import { MoviesApi, TVApi } from "../../api";
 
 export default class extends React.Component {
   state = {
@@ -9,8 +10,35 @@ export default class extends React.Component {
     loading: false,
     error: null
   };
+
+  handleSubmit = () => {
+    const { searchTerm } = this.state;
+    if (searchTerm !== "") {
+      this.searchByTerm();
+    }
+  };
+
+  searchByTerm = async () => {
+    const { searchTerm } = this.state;
+    try {
+      const {
+        data: { results: movieResults }
+      } = await MoviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults }
+      } = await TVApi.search(searchTerm);
+      this.setState({ movieResults, tvResults });
+      this.setState({ loading: true });
+    } catch {
+      this.setState({ error: "Can't find results." });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
+
   render() {
     const { movieResults, tvResults, searchTerm, loading, error } = this.state;
+    console.log(this.state);
     return (
       <SearchPresenter
         movieResults={movieResults}
@@ -18,6 +46,7 @@ export default class extends React.Component {
         SearchPresenter={searchTerm}
         loading={loading}
         error={error}
+        handleSubmit={this.handleSubmit}
       />
     );
   }
