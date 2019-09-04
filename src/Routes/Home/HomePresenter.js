519da7f249ba9c8f0f029e6d30bedba5d373da56
miniyoung37 <miniyoung37@gmail.com>
2019-09-05 00:06:18 +0900

    styled css

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,11 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Section from "Components/Section";
+
+const Container = styled.div`
+  padding: 0px 10px;
+`;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-  null;
+  loading ? null : (
+    <Container>
+      {nowPlaying && nowPlaying.length > 0 && (
+        <Section title="Now Playing">
+          {nowPlaying.map(movie => movie.title)}
+        </Section>
+      )}
+      {upcoming && upcoming.length > 0 && (
+        <Section title="Upcoming Movies">
+          {upcoming.map(movie => movie.title)}
+        </Section>
+      )}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Movies">
+          {popular.map(movie => movie.title)}
+        </Section>
+      )}
+    </Container>
+  );
 
-HomePresenter.PropTypes = {
+HomePresenter.propTypes = {
   nowPlaying: PropTypes.array,
   popular: PropTypes.array,
   upcoming: PropTypes.array,
