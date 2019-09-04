258d6ca815436632b79e410054cd98ecf68b0293
miniyoung37 <miniyoung37@gmail.com>
2019-09-04 21:35:41 +0900

    DetailContainer 작업

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -8,8 +8,21 @@ export default class extends React.Component {
     loading: true
   };
 
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id }
+      },
+      history: { push }
+    } = this.props;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
+    }
+  }
+
   render() {
-    const { result, eroor, loading } = this.state;
+    const { result, error, loading } = this.state;
     return <DetailPresenter result={result} error={error} loading={loading} />;
   }
 }
