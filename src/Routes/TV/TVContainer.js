e14661bafccc93e4b7e2f73df5d5445b293282f5
miniyoung37 <miniyoung37@gmail.com>
2019-09-04 20:55:43 +0900

    TVcontainer 수정

--- src/Routes/TV/TVContainer.js
+++ src/Routes/TV/TVContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TVPresenter from "./TVPresenter";
+import { TVApi } from "api";
 
 export default class extends React.Component {
   state = {
@@ -9,6 +10,28 @@ export default class extends React.Component {
     loading: true,
     error: null
   };
+
+  async componentDidMount() {
+    try {
+      const {
+        data: { results: topRated }
+      } = await TVApi.topRated();
+      const {
+        data: { results: popular }
+      } = await TVApi.popular();
+      const {
+        data: { results: airingToday }
+      } = await TVApi.airingToday();
+      this.setState({ topRated, popular, airingToday });
+    } catch {
+      this.setState({
+        error: "Can't find TV information"
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
   render() {
     const { topRated, popular, airingToday, loading, error } = this.state;
     return (
