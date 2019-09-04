f44c3e73a094e4720b18db3914cb7b26d3129842
miniyoung37 <miniyoung37@gmail.com>
2019-09-04 21:59:45 +0900

    Detail 수정중

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,12 +1,20 @@
 import React from "react";
 import DetailPresenter from "./DetailPresenter";
+import { MoviesApi, TVApi } from "../../api";
 
 export default class extends React.Component {
-  state = {
-    result: null,
-    error: null,
-    loading: true
-  };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname }
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/")
+    };
+  }
 
   async componentDidMount() {
     const {
@@ -15,9 +23,24 @@ export default class extends React.Component {
       },
       history: { push }
     } = this.props;
+    const { isMovie } = this.state; // url 주소 검사
     const parsedId = parseInt(id);
     if (isNaN(parsedId)) {
       return push("/");
+    }
+    let result = null;
+    try {
+      if (isMovie) {
+        const request = await MoviesApi.movieDetail(parsedId);
+        result = request.data;
+      } else {
+        const request = await TVApi.showDetail(parsedId);
+        result = request.data;
+      }
+    } catch {
+      this.setState({ error: "Can't find anything." });
+    } finally {
+      this.setState({ loading: false, result });
     }
   }
 
