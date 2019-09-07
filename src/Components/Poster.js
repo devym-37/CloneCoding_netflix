2ab1ba723c7cfe19dc091e0b7a1a72e4a4a98f1d
miniyoung37 <miniyoung37@gmail.com>
2019-09-07 18:18:36 +0900

    poster works

--- src/Components/Poster.js
+++ src/Components/Poster.js
@@ -3,31 +3,71 @@ import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-const Container = styled.div``;
+const Container = styled.div`
+  font-size: 12px;
+`;
 
-const IamgeContainer = styled.div``;
+const Image = styled.div`
+  background-image: url(${props => props.bgUrl});
+  height: 180px;
+  background-size: cover;
+  border-radius: 4px;
+  background-position: center center;
+  transition: opacity 0.1s ease-in-out;
+`;
 
-const Image = styled.div``;
+const Rating = styled.span`
+  bottom: 5px;
+  position: absolute;
+  right: 5px;
+  opacity: 0;
+  transition: opacity 0.1s ease-in-out;
+`;
 
-const Rating = styled.span``;
+const Title = styled.span`
+  display: block;
+  margin-bottom: 3px;
+`;
 
-const Title = styled.span``;
+const Year = styled.span`
+  font-size: 10px;
+  color: rgba(255, 255, 255, 0.5);
+`;
 
-const Year = styled.span``;
+const ImageContainer = styled.div`
+  margin-bottom: 5px;
+  position: relative;
+  &:hover {
+    ${Image} {
+      opacity: 0.3;
+    }
+    ${Rating} {
+      opacity: 1;
+    }
+  }
+`;
 
 const Poster = ({ id, imageUrl, title, rating, year, isMovie = false }) => (
   <Link to={isMovie ? `/movie/${id}` : `/show/${id}`}>
     <Container>
-      <IamgeContainer>
-        <Image bgUrl={imageUrl} />
+      <ImageContainer>
+        <Image
+          bgUrl={
+            imageUrl
+              ? `http://image.tmdb.org/t/p/w300${imageUrl}`
+              : require("../assets/Noimage.png")
+          }
+        />
         <Rating>
           <span role="img" aria-label="rating">
             ★
           </span>{" "}
           {rating}/10
         </Rating>
-      </IamgeContainer>
-      <Title>{title}</Title>
+      </ImageContainer>
+      <Title>
+        {title.length > 18 ? `${title.substring(0, 18)}...` : title}
+      </Title>
       <Year>{year}</Year>
     </Container>
   </Link>
