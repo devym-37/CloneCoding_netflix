e3af00db54998d8bfe4edb97803f8ea5c84c7354
LeeYoungMin <miniyoung37@gmail.com>
2020-03-24 01:49:35 +0900

    Updates

---
+++ precache-manifest.91267713be8ee20201d0e2604d990797.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14394f2c2afe6b4bdb263977ac667587",
+    "url": "/nomflix/index.html"
+  },
+  {
+    "revision": "aa03383706b937bbf161",
+    "url": "/nomflix/static/js/2.ccf25582.chunk.js"
+  },
+  {
+    "revision": "06a8528bab2d8d448e30",
+    "url": "/nomflix/static/js/main.0933ca79.chunk.js"
+  },
+  {
+    "revision": "767bd30a57c9579b580e",
+    "url": "/nomflix/static/js/runtime~main.cbbd33c8.js"
+  },
+  {
+    "revision": "a511bad435fb2551ebf3595c462b1009",
+    "url": "/nomflix/static/media/Noimage.a511bad4.png"
+  }
+]);
